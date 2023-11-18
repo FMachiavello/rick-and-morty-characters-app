595f69da49609c74cabcaeda838535dbc814ed9e
fmachiavello <franco.machiavello@hotmail.com>
2023-11-18 20:36:26 -0300

    Modularizing the post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,67 +1,15 @@
-import React from 'react';
-import { View, Image, Text, StyleSheet, FlatList } from 'react-native';
-import StateDot from './StateDot';
-import SaveButton from './SaveButton';
-const Post = ({ characters }) => {
+import React from "react";
+import { FlatList } from "react-native";
+import { PostItem } from "./PostItem";
 
+const Post = ({ characters }) => {
   return (
     <FlatList
       data={characters}
       keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <View style={styles.postContainer}>
-          <Image style={styles.postImage} source={{ uri: item.image }}/>
-          <View style={styles.titleContainer}>
-            <Text style={styles.postTitle} >{item.name}</Text>
-            <SaveButton/>
-          </View>
-          <StateDot status={item.status}/>
-          <Text style={styles.postMeta}>Last seen:</Text>
-          <Text style={styles.postDescription} >{item.location.name}</Text>
-          <Text style={styles.postMeta}>First seen:</Text>
-          <Text style={styles.postDescription} >{item.origin.name}</Text>
-        </View>
-        )}
-      />
+      renderItem={({ item }) => <PostItem item={item} />}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  titleContainer:{
-    flexDirection:'row',
-    flex:1,
-    alignItems:'center',
-    justifyContent:'space-between',
-  
-  },
-  postContainer: {
-    maxWidth: 800,
-    margin: 20,
-    backgroundColor: '#fff',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-  }},
-  postImage: {
-    padding: 30,
-    width: 300,
-    height:300,
-    borderRadius: 8,
-    marginBottom: 10,
-  },
-  postTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    paddingTop:5,
-    paddingBottom:10,
-    flexDirection:'row'
-
-  },
-  postMeta: {
-    fontSize: 14,
-    color: '#777',
-  },
-});
 
 export default Post;
